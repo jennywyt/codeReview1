0c654edded57210ab1e478e1257e001ef1f44634
Jenny Wong <yintingjenny.wong@gmail.com>
2020-10-31 14:14:19 +0100

    Update

--- src/Lastquestion.java
+++ src/Lastquestion.java
@@ -1,21 +1,19 @@
 import java.util.Scanner;
 
-public class lastquestion {
+public class Lastquestion {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         if (args.length < 2) {
             System.err.println("Please enter two numbers");
-        }
-        else {
+        } else {
             for (int i = 0; i < args.length; i++) {
                 System.out.println(args[i]);
             }
             int width = Integer.parseInt(args[0]);
             int height = Integer.parseInt(args[1]);
 
-            Q1.calculateRectArea(width, height);
+            System.out.println("The rectangle area is " + Q1.calculateRectArea(width, height));
         }
-
     }
 }
--- src/Q1.java
+++ src/Q1.java
@@ -1,18 +1,19 @@
 public class Q1 {
 
-    public static void calculateRectArea(int width, int height) {
+    public static int calculateRectArea(int width, int height) {
         int area = width * height;
-        System.out.println("The rectangle area is "+ area);
+        return area;
     }
 
-    static void calculateSquare(int side) {
+    static int calculateSquare(int side) {
         int squareArea = side * side;
-        System.out.println("The square area is " + squareArea);
+        return squareArea;
     }
 
-    static void calculatesquarePerimeter(int side) {
+    static int calculatesquarePerimeter(int side) {
         int squarePerimeter = side * 4;
-        System.out.println("The square perimeter is "+ squarePerimeter);
+        return squarePerimeter;
+
     }
 
     public static void main(String[] args) {
@@ -23,23 +24,23 @@ public class Q1 {
         System.out.println(area);
 
         int[][] rectangles = {
-                {20,30},{40,50},{80,40},{88,5},{100,9}
+                {20, 30}, {40, 50}, {80, 40}, {88, 5}, {100, 9}
 
         };
 
-        for(int i =0; i < rectangles.length; i++){
-            calculateRectArea(rectangles[i][0],rectangles[i][1]);
+        for (int i = 0; i < rectangles.length; i++) {
+            System.out.println("Rectangle with width of " + rectangles[i][0] + " and the length of " + rectangles[i][1] + ":");
+            System.out.println("The rectangle area is " + calculateRectArea(rectangles[i][0], rectangles[i][1]));
         }
 
-        int [][]square = {
-                {4},{80},{66},{1000},{42},{588}
+        int[][] square = {
+                {4}, {80}, {66}, {1000}, {42}, {588}
         };
 
-        for(int i =0; i<square.length;i++){
-            calculatesquarePerimeter(square[i][0]);
-            calculateSquare(square[i][0]);
-        };
-
-
+        for (int i = 0; i < square.length; i++) {
+            System.out.println("Square with width of " + square[i][0] +":");
+            System.out.println("The square perimeter is " + calculatesquarePerimeter(square[i][0]));
+            System.out.println("The square area is " + calculateSquare(square[i][0]));
+        }
     }
 }
